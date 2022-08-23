ea19e305f0be2c11698189d28e59428cc872ff37
Pedro Paulo <pe.pn03@gmail.com>
2022-08-23 12:22:08 -0300

    First commit

---
+++ Makefile
@@ -0,0 +1,47 @@
+SOURCES =	main.c
+NAME = minishell
+LIBFT = libft/libft.a
+SRCS_PATH = srcs
+OBJS_PATH = objs
+SRCS = ${addprefix ${SRCS_PATH}/, ${SOURCES}}
+OBJS = ${addprefix ${OBJS_PATH}/, ${SOURCES:.c=.o}}
+CC = gcc
+FLAGS =  -Werror -Wextra -Wall
+
+# Colors
+GREEN = \33[1;32m
+L_CYAN = \33[1;36m
+L_BLUE = \33[1;34m
+L_GREEN = \33[1;92m
+L_PURPLE = \33[1;35m
+NC = \033[0m
+
+all: ${NAME}
+
+bonus: ${BONUS}
+
+${OBJS_PATH}/%.o: ${SRCS_PATH}/%.c
+					@ mkdir -p ${OBJS_PATH}
+					@ echo "Compiling: $<"
+					@ ${CC} ${FLAGS} -c $< -o $@ -lreadline -I libft/include/ -I include/
+
+${NAME}: ${LIBFT} ${OBJS}
+			@ ${CC} ${FLAGS} ${OBJS} ${LIBFT} -o ${NAME}
+			@ echo "${GREEN}${NAME} successfully compiled!${NC}"
+
+${LIBFT}: 
+		@ make -C libft/ --no-print-directory
+
+clean:
+		@ rm -rf ${OBJS_PATH} ${BOBJS_PATH}
+		@ echo "${L_CYAN}Objects deleted!${NC}"
+
+fclean: clean
+		@ make fclean -C libft/ --no-print-directory
+		@ rm -rf ${NAME} ${BONUS}
+		@ echo "${L_BLUE}push_swap deleted!${NC}"
+	
+re: fclean all
+
+norma:
+		norminette ${SRCS} include/
---
+++ include/minishell.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ppaulo-d <ppaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/23 11:12:09 by ppaulo-d          #+#    #+#             */
+/*   Updated: 2022/08/23 12:07:31 by ppaulo-d         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include "libft.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#endif
