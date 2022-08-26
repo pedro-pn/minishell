7e4a86dc6408a25e9739a839befc41889081b8a7
Pedro Paulo <pe.pn03@gmail.com>
2022-08-26 14:47:04 -0300

    Update libft

--- libft/include/libft.h
+++ libft/include/libft.h
@@ -58,6 +58,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_strcmp(const char *s1, const char *s2);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
