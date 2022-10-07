ca623203b10df2ae85f46c9283b34229d9a07f75
Pedro Paulo <pe.pn03@gmail.com>
2022-10-07 12:47:57 -0300

    Undo unset last modification

--- srcs/built_in/unset.c
+++ srcs/built_in/unset.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: frosa-ma <frosa-ma@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: ppaulo-d <ppaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/01 05:43:46 by frosa-ma          #+#    #+#             */
-/*   Updated: 2022/10/07 11:03:48 by frosa-ma         ###   ########.fr       */
+/*   Updated: 2022/10/07 12:29:52 by ppaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,6 @@ int	__unset(char **args, t_data *data)
 	while (index++, args[index])
 	{
 		node = ft_lstfind(data->lst_env, args[index]);
-		if (!node)
-			return (1);
 		empty_node = ft_lstfind_2(data->empty_vars, args[index]);
 		if (empty_node)
 			ft_lstremove_2(&data->empty_vars, args[index]);
